d2a6ff090e1c7609d612eb4ed8bbbbffdfe86b9d
David Pam <niratpam@gmail.com>
2020-10-17 02:41:09 +0100

    Fixing 'REMOVE_INGREDIENT' Logical Error

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -35,8 +35,7 @@ const reducer = (state = initialState, action) => {
           ...state.ingredients,
           [action.ingredientName]: state.ingredients[action.ingredientName] - 1,
         },
-        totalPrice: (state.totalPrice =
-          INGREDIENT_PRICES[action.ingredientName]),
+        totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredientName],
       };
     default:
       return state;
