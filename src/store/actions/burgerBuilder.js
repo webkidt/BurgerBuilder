b846d39d11001292d696c14bc481095d5ad4f043
David Pam <niratpam@gmail.com>
2020-10-30 00:14:59 +0100

    Refactoring Reducers Continued

--- src/store/actions/burgerBuilder.js
+++ src/store/actions/burgerBuilder.js
@@ -17,7 +17,7 @@ export const removeIngredient = (name) => {
 
 export const setIngredients = (ingredients) => {
   return {
-    type: actionTypes.SET_INGREDIENT,
+    type: actionTypes.SET_INGREDIENTS,
     ingredients: ingredients,
   };
 };
