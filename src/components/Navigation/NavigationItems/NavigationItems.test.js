8b50fea32637b36f14b2cbbdb3e77d7a71fa81e7
David Pam <niratpam@gmail.com>
2020-11-05 21:33:59 +0100

    Jest and Enzyme Documentations

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -22,4 +22,9 @@ describe('<NavigationItems />', () => {
     wrapper.setProps({ isAuthenticated: true });
     expect(wrapper.find(NavigationItem)).toHaveLength(3);
   });
+
+  it('should contain Logout NavigationItem element if authenticated', () => {
+    wrapper.setProps({ isAuthenticated: true });
+    expect(wrapper.contains(<NavigationItem link='/logout'>Logout</NavigationItem>)).toEqual(true);
+  });
 });
